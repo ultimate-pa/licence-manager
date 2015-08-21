e0eba90cd7062818e6c83bb89a447b8e33ea5e81
Daniel Dietsch <dietsch@informatik.uni-freiburg.de>
2015-08-21 16:22:23 -0400

    - its working

--- src/main/java/de/uni_freiburg/informatik/ultimate/licence_manager/LicenceManager.java
+++ src/main/java/de/uni_freiburg/informatik/ultimate/licence_manager/LicenceManager.java
@@ -1,11 +1,40 @@
+/*
+ * Copyright (C) 2015 University of Freiburg
+ * Copyright (C) 2015 dietsch@informatik.uni-freiburg.de
+ * 
+ * This file is part of the ULTIMATE licence-manager.
+ * 
+ * The ULTIMATE licence-manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * The ULTIMATE licence-manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE licence-manager. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE licence-manager, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
+ * containing parts covered by the terms of the Eclipse Public License, the 
+ * licensors of the ULTIMATE licence-manager grant you additional permission 
+ * to convey the resulting work.
+ */
 package de.uni_freiburg.informatik.ultimate.licence_manager;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Collection;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import de.uni_freiburg.informatik.ultimate.licence_manager.exception.RuntimeIOException;
 import de.uni_freiburg.informatik.ultimate.licence_manager.util.FileUtils;
 
 /**
@@ -26,8 +55,20 @@ public final class LicenceManager {
 		mTemplateName = templatename;
 	}
 
+	public void writeDry() {
+		consumeAll(getWriteConsumer(getLimitedPrintConsumer()));
+	}
+
+	public void deleteDry() {
+		consumeAll(getDeleteConsumer(getLimitedPrintConsumer()));
+	}
+
 	public void delete() {
-		consumeAll(getDryRunDeleteConsumer(5));
+		consumeAll(getDeleteConsumer(getWriteConsumer()));
+	}
+
+	public void write() {
+		consumeAll(getWriteConsumer(getWriteConsumer()));
 	}
 
 	private void consumeAll(final Consumer<LicencedFile> consumer) {
@@ -59,39 +100,60 @@ public final class LicenceManager {
 		return FileUtils.getFiles(mDirectory, mFileendings);
 	}
 
-	private Consumer<LicencedFile> getDryRunWriteConsumer(int limit) {
+	private Consumer<LicencedFile> getWriteConsumer(
+			Consumer<FileAndStream> contentProcessor) {
 		return fileToLicence -> {
 			System.out.print(fileToLicence);
 			if (!fileToLicence.needsWriting()) {
 				System.out.println("..... will not be changed.");
 				return;
 			}
-			System.out.println("..... licence will be changed/added. Result:");
-			if (limit > 0) {
-				fileToLicence.getNewContent().limit(limit)
-						.forEach(System.out::println);
-			} else {
-				fileToLicence.getNewContent().forEach(System.out::println);
-			}
-			System.out.println("[...]");
+			System.out.println("..... licence will be changed/added.");
+			contentProcessor.accept(new FileAndStream(fileToLicence.getFile(),
+					fileToLicence.getNewContent()));
 		};
 	}
 
-	private Consumer<LicencedFile> getDryRunDeleteConsumer(int limit) {
+	private Consumer<LicencedFile> getDeleteConsumer(
+			Consumer<FileAndStream> consumer) {
 		return fileToLicence -> {
 			System.out.print(fileToLicence);
 			if (!fileToLicence.hasLicence()) {
 				System.out.println("..... will not be changed.");
 				return;
 			}
-			System.out.println("..... licence will be deleted. Result:");
-			if (limit > 0) {
-				fileToLicence.getContentWithoutLicence().limit(limit)
-						.forEach(System.out::println);
-			} else {
-				fileToLicence.getContentWithoutLicence().forEach(System.out::println);
-			}
+			System.out.println("..... licence will be deleted.");
+			consumer.accept(new FileAndStream(fileToLicence.getFile(),
+					fileToLicence.getContentWithoutLicence()));
+		};
+	}
+
+	private Consumer<FileAndStream> getLimitedPrintConsumer() {
+		return fs -> {
+			System.out.println("Result:");
+			fs.Stream.limit(5).forEach(System.out::println);
 			System.out.println("[...]");
 		};
+	}
+
+	private Consumer<FileAndStream> getWriteConsumer() {
+		return fs -> {
+			try {
+				Files.write(fs.File.toPath(),
+						fs.Stream.collect(Collectors.toList()));
+			} catch (IOException e) {
+				throw new RuntimeIOException(e);
+			}
+		};
+	}
+
+	private static class FileAndStream {
+		private Stream<String> Stream;
+		private File File;
+
+		private FileAndStream(File file, Stream<String> stream) {
+			File = file;
+			Stream = stream;
+		}
 	}
 }
--- src/main/java/de/uni_freiburg/informatik/ultimate/licence_manager/Main.java
+++ src/main/java/de/uni_freiburg/informatik/ultimate/licence_manager/Main.java
@@ -1,3 +1,29 @@
+/*
+ * Copyright (C) 2015 University of Freiburg
+ * Copyright (C) 2015 dietsch@informatik.uni-freiburg.de
+ * 
+ * This file is part of the ULTIMATE licence-manager.
+ * 
+ * The ULTIMATE licence-manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * The ULTIMATE licence-manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE licence-manager. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE licence-manager, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
+ * containing parts covered by the terms of the Eclipse Public License, the 
+ * licensors of the ULTIMATE licence-manager grant you additional permission 
+ * to convey the resulting work.
+ */
 package de.uni_freiburg.informatik.ultimate.licence_manager;
 
 import org.apache.commons.cli.CommandLine;
@@ -13,6 +39,12 @@ import org.apache.commons.cli.ParseException;
  */
 public class Main {
 
+	private static final String OPTION_DIRECTORY = "directory";
+	private static final String OPTION_TEMPLATE_NAME = "name";
+	private static final String OPTION_DRY_RUN = "dry-run";
+	private static final String OPTION_HELP = "help";
+	private static final String OPTION_DELETE = "delete";
+
 	public static void main(final String[] args) {
 		final Options cliOptions = createOptions();
 		final CommandLine cmds = parseArguments(args, cliOptions);
@@ -21,30 +53,55 @@ public class Main {
 			return;
 		}
 
-		if (cmds.hasOption("h")) {
+		if (cmds.hasOption(OPTION_HELP)) {
 			printHelp(cliOptions);
 			System.exit(0);
 			return;
 		}
 
-		if (!cmds.hasOption("d")) {
-			System.err.print("Missing parameter \"directory\"");
-			printHelp(cliOptions);
+		// check required parameters
+		final String[] requiredOptions = new String[] { OPTION_DIRECTORY,
+				OPTION_TEMPLATE_NAME };
+		for (final String requiredOption : requiredOptions) {
+			if (!cmds.hasOption(requiredOption)) {
+				System.err
+						.print("Missing parameter \"" + requiredOption + "\"");
+				printHelp(cliOptions);
+				System.exit(1);
+				return;
+			}
+		}
+
+		try {
+
+			final String[] fileendings = new String[] { ".java", "pom.xml" };
+			final LicenceManager licenceManager = new LicenceManager(
+					cmds.getOptionValue(OPTION_DIRECTORY), fileendings,
+					cmds.getOptionValue(OPTION_TEMPLATE_NAME));
+
+			if (cmds.hasOption(OPTION_DRY_RUN)) {
+				System.out.println("This is a dry run:");
+				if (cmds.hasOption(OPTION_DELETE)) {
+					licenceManager.deleteDry();
+				} else {
+					licenceManager.writeDry();
+				}
+			} else {
+				if (cmds.hasOption(OPTION_DELETE)) {
+					licenceManager.delete();
+				} else {
+					licenceManager.write();
+				}
+			}
+
+			System.exit(0);
+			return;
+		} catch (Exception ex) {
+			System.err.println("Unexpected error: " + ex.getMessage());
 			System.exit(1);
 			return;
 		}
 
-		if (!cmds.hasOption("n")) {
-			System.err.print("Missing parameter \"name\"");
-			printHelp(cliOptions);
-			System.exit(1);
-			return;
-		}
-
-		final String[] fileendings = new String[] { ".java", "pom.xml" };
-		final LicenceManager licenceManager = new LicenceManager(
-				cmds.getOptionValue("d"), fileendings, cmds.getOptionValue("n"));
-		licenceManager.delete();
 	}
 
 	private static CommandLine parseArguments(final String[] args,
@@ -68,19 +125,25 @@ public class Main {
 	private static Options createOptions() {
 		final Options cliOptions = new Options();
 
-		cliOptions.addOption(Option.builder("d").longOpt("directory")
+		cliOptions.addOption(Option.builder("d").longOpt(OPTION_DIRECTORY)
 				.desc("specify the directory that should be parsed")
 				.argName("dir").hasArg().build());
 
-		cliOptions.addOption(Option.builder("n").longOpt("name")
+		cliOptions.addOption(Option.builder("n").longOpt(OPTION_TEMPLATE_NAME)
 				.desc("specify the directory that should be parsed")
 				.argName("dir").hasArg().build());
 
-		cliOptions.addOption(Option.builder("del").longOpt("delete")
-				.desc("removes all licence notices from the specified files")
-				.build());
+		cliOptions.addOption(Option.builder().longOpt(OPTION_DELETE)
+				.desc("delete all licence texts").build());
 
-		cliOptions.addOption("h", "help", false, "prints this message");
+		cliOptions
+				.addOption(Option
+						.builder()
+						.longOpt(OPTION_DRY_RUN)
+						.desc("do not perform any actual change, just print what would happen")
+						.build());
+
+		cliOptions.addOption("h", OPTION_HELP, false, "prints this message");
 
 		return cliOptions;
 	}
